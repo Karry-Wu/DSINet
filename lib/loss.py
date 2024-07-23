21b8fb82f2ee3c21ce3c1d336f0674ab007382e7
Karry-Wu <117814187+Karry-Wu@users.noreply.github.com>
2024-07-23 19:55:06 +0800

    Add files via upload

---
+++ lib/loss.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class IOU(torch.nn.Module):
+    def __init__(self):
+        super(IOU, self).__init__()
+
+    def _iou(self, pred, target):
+        pred = torch.sigmoid(pred)
+        inter = (pred * target).sum(dim=(2, 3))
+        union = (pred + target).sum(dim=(2, 3)) - inter
+        iou = 1 - (inter / union)
+
+        return iou.mean()
+
+    def forward(self, pred, target):
+        return self._iou(pred, target)
+
+class structure_loss(torch.nn.Module):
+    def __init__(self):
+        super(structure_loss, self).__init__()
+
+    def _structure_loss(self, pred, mask):
+        weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+        wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+        wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+
+        pred = torch.sigmoid(pred)
+        inter = ((pred * mask) * weit).sum(dim=(2, 3))
+        union = ((pred + mask) * weit).sum(dim=(2, 3))
+        wiou = 1 - (inter) / (union - inter)
+        return (wbce + wiou).mean()
+
+    def forward(self, pred, mask):
+        return self._structure_loss(pred, mask)
