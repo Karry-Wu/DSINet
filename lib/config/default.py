1fa40305ca999c6c17288b2486b94504bbcf9dd2
Karry-Wu <117814187+Karry-Wu@users.noreply.github.com>
2024-09-27 15:22:40 +0800

    Update default.py

--- lib/config/default.py
+++ lib/config/default.py
@@ -1,11 +1,4 @@
 
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# Modified by Ke Sun (sunk@mail.ustc.edu.cn)
-# ------------------------------------------------------------------------------
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
